af50cbd70b04ab9e2b89c1977a8c86b8b716b89f
Siti Zulaikha <zulaikha.affandi@gmail.com>
2026-04-09 23:31:10 +0800

    Initial commit - Project started at 2026-04-09T14:52:39Z

---
+++ src/main/java/com/hospital_patient_management_api/HospitalPatientManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.hospital_patient_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPatientManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalPatientManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital_patient_management_api/HospitalPatientManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_patient_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPatientManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
