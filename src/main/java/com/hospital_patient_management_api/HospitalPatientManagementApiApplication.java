ae089de0204d10c4e3c4a145e3a34d4c0b0cb2cc
Siti Zulaikha <zulaikha.affandi@gmail.com>
2026-04-19 16:21:01 +0800

    config: add Wildfly WAR deployment support

--- src/main/java/com/hospital_patient_management_api/HospitalPatientManagementApiApplication.java
+++ src/main/java/com/hospital_patient_management_api/HospitalPatientManagementApiApplication.java
@@ -2,9 +2,17 @@ package com.hospital_patient_management_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HospitalPatientManagementApiApplication {
+//public class HospitalPatientManagementApiApplication {
+public class HospitalPatientManagementApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HospitalPatientManagementApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalPatientManagementApiApplication.class, args);
